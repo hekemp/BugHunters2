5be70f4dd06ac27b610582e8fa0f15cb4c1570fd
Alic Szecsei <aszecsei@gmail.com>
2018-02-13 19:39:09 -0600

    Initial commit

---
+++ Assets/CameraMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour {
+    public float radius = 10f;
+    public float speed = 0.1f;
+    private float theta = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // TODO: Alter theta based on player lean
+        theta += Time.deltaTime * speed;
+
+        this.transform.position = new Vector3(Mathf.Sin(theta) * radius, 0, Mathf.Cos(theta) * radius);
+        this.transform.rotation = Quaternion.Euler(0, Mathf.Rad2Deg * theta, 0);
+	}
+}
