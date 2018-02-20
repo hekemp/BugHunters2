643729a25b9487e15968852ad3e0ff50a81f054d
vruser <VRUser@SAAH-S273.iowa.uiowa.edu>
2018-02-20 16:42:33 -0600

    Add movement

--- Assets/Bughunters 2/Scripts/CameraMover.cs
+++ Assets/Bughunters 2/Scripts/CameraMover.cs
@@ -7,17 +7,46 @@ public class CameraMover : MonoBehaviour {
     public float speed = 0.1f;
     private float theta = 0f;
 
+    public Transform player;
+    public Transform headset;
+
+    private Vector3 origin;
+    private Vector3 initialPosition;
+
 	// Use this for initialization
 	void Start () {
-		
+        initialPosition = transform.position;
+        ResetPosition();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        // TODO: Alter theta based on player lean
-        theta += Time.deltaTime * speed;
+        if(Input.GetKey(KeyCode.JoystickButton0) && Input.GetKey(KeyCode.JoystickButton2))
+        {
+            ResetPosition();
+        }
 
-        this.transform.position = new Vector3(Mathf.Sin(theta) * radius, 0, Mathf.Cos(theta) * radius);
+        // TODO: This changes rotation based on where you're looking...might need to fix
+        Vector3 diff = Quaternion.Inverse(headset.localRotation) * (headset.localPosition - origin);
+        Debug.Log(diff.x);
+        if (Mathf.Abs(diff.x) > 0.05f)
+        {
+            theta += Time.deltaTime * speed * (diff.x - (Mathf.Sign(diff.x) * 0.05f));
+        }
+
+        this.transform.position = new Vector3(Mathf.Sin(theta) * radius, 0, Mathf.Cos(theta) * radius) + initialPosition;
         this.transform.rotation = Quaternion.Euler(0, Mathf.Rad2Deg * theta, 0);
 	}
+
+    void ResetPosition()
+    {
+        Debug.Log("Reset!");
+        Vector3 diff = headset.position - transform.position;
+        diff.y = 0;
+        player.position -= diff;
+        diff = headset.rotation.eulerAngles - transform.rotation.eulerAngles;
+        diff.x = diff.z = 0;
+        player.rotation = Quaternion.Euler(player.rotation.eulerAngles - diff);
+        origin = headset.localPosition;
+    }
 }
